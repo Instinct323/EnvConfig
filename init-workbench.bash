5f06206b4d9ef81ae89aa7153f689212e3a5a706
tongzj <tongzanjia@qq.com>
2025-05-18 17:11:24 +0800

    update on 2025-05-18 17:11:24.102138

--- init-workbench.bash
+++ init-workbench.bash
@@ -5,8 +5,11 @@ cd Workbench
 mkdir asssets
 mkdir Lab
 
-git clone git@github.com:Instinct323/cppmod.git
-git clone git@github.com:Instinct323/pymod.git
-git clone git@github.com:Instinct323/ModelsAPI.git
-git clone git@github.com:Instinct323/Instinct323.git notes
-git clone git@github.com:Instinct323/EnvConfig.git
+# export URL_BASE=https://github.com/Instinct323
+export URL_BASE=git@github.com:Instinct323
+
+git clone $URL_BASE/cppmod.git
+git clone $URL_BASE/pymod.git
+git clone $URL_BASE/ModelsAPI.git
+git clone $URL_BASE/.git notes
+git clone $URL_BASE/EnvConfig.git
