e52e99528e2df720b654598432f58b4a0ff5bce5
tongzj <tongzanjia@qq.com>
2025-05-19 10:17:44 +0800

    update on 2025-05-19 10:17:44.227580

---
+++ init-disk.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+# init-disk.bash <disk-path>
+cd $1
+
+# export URL_BASE=https://github.com/Instinct323
+export URL_BASE=git@github.com:Instinct323
+
+# --- Downloads
+mkdir Downloads
+
+# --- Information
+mkdir Information
+git clone $URL_BASE/.git Information/notes
+
+# --- Workbench ---
+mkdir Workbench
+cd Workbench
+mkdir asssets
+mkdir Lab
+
+git clone $URL_BASE/cppmod.git
+git clone $URL_BASE/pymod.git
+git clone $URL_BASE/ModelsAPI.git
+git clone $URL_BASE/EnvConfig.git
