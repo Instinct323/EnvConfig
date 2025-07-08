769412ac2a702bce7e6c9dddfe65c0851c3416ec
tongzj <tongzanjia@qq.com>
2025-07-08 14:40:46 +0800

    update on 2025-07-08 14:40:46.735179

--- ubuntu/startup.bash
+++ ubuntu/startup.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 gnome-terminal \
+  --tab --title="NviTop" -e "bash -c 'nvitop; exec bash'" \
   --tab --title="ROS core" -e "bash -c 'roscore; exec bash'" \
-  --tab --title="Zotero" -e "bash -c 'zotero; exec bash'" \
-  --tab --title="NviTop" -e "bash -c 'nvitop; exec bash'"
+  --tab --title="Zotero" -e "bash -c '/opt/Zotero/zotero; exec bash'" \
