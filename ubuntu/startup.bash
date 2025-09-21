0856c90edb8928c83d23c8553ccf08b512c831af
tongzj <tongzanjia@qq.com>
2025-09-21 20:05:42 +0800

    update on 2025-09-21 20:05:42.576767

--- ubuntu/startup.bash
+++ ubuntu/startup.bash
@@ -3,4 +3,7 @@
 gnome-terminal \
   --tab --title="NviTop" -e "bash -c 'nvitop; exec bash'" \
   --tab --title="Zotero" -e "bash -c '/opt/Zotero/zotero; exec bash'" \
-  # --tab --title="ROS core" -e "bash -c 'roscore; exec bash'" \
+
+if [ "$ROS_VERSION" = "1" ]; then
+  gnome-terminal --tab --title="ROS core" -e "bash -c 'roscore; exec bash'"
+fi
